ba65e028d8294da628f26de02807885c90d4108c
jhyang22 <jaeho9229@gmail.com>
2025-04-20 15:01:45 +0900

    refactor : 레벨 3-1 : 테스트 코드 연습 - 1

--- src/main/java/org/example/expert/config/PasswordEncoder.java
+++ src/main/java/org/example/expert/config/PasswordEncoder.java
@@ -11,6 +11,14 @@ public class PasswordEncoder {
     }
 
     public boolean matches(String rawPassword, String encodedPassword) {
+        // rawPassword.toCharArray() : 로그인 시 입력한 비밀번호
+        // 이 때 toCharArray()를 사용한 이유
+        // - String은 불변객체라서 GC가 처리해주기 전까지는 메모리에 남아있을 수 있다
+        //      - String s = "hello"; 이후 s = "world"; 라고 수정하면 기존의 문자열은 그대로 있고 s의 참조값이 바뀌는 것!
+        // - char[]은 초기화가 가능! 비밀번호 검증 후에 로그인 시 사용한 비밀번호는 초기화하여 흔적을 지워버린다!
+        //      - 근데 배열로 만들면 어케 비교하지? 싶었는데 BCrypt라는애가 알아서 비교해주는 듯!
+        // BCrypt.verifyer().verify()
+        // - rarPassword와 encodedPassword랑 비교해서 BCrypt.Result라는 boolean 값이 나온다
         BCrypt.Result result = BCrypt.verifyer().verify(rawPassword.toCharArray(), encodedPassword);
         return result.verified;
     }
--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -15,14 +15,20 @@ class PasswordEncoderTest {
 
     @Test
     void matches_메서드가_정상적으로_동작한다() {
-        // given
+        // given - 준비
+        // 두 매개변수를 임의로 준비함
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        /**
+         * 레벨 3-1 테스트 코드 연습 - 1
+         */
+        // when - 실행
+        // given에서 만들어놓은 매개변수를 넣어서 실행
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
-        // then
+        // then - 결과
+        // () 안의 조건이 true라면 test 통과!라는 메서드
         assertTrue(matches);
     }
 }
