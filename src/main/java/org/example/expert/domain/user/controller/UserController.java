b5f5d29786be0af4b1fc85b086585386c46a308f
jhyang22 <jaeho9229@gmail.com>
2025-04-20 16:50:44 +0900

    refactor : 레벨 1-3 : Validation - @Valid 누락부분 추가

--- src/main/java/org/example/expert/domain/user/controller/UserController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package org.example.expert.domain.user.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
@@ -20,8 +21,16 @@ public class UserController {
         return ResponseEntity.ok(userService.getUser(userId));
     }
 
+    /**
+     * 과제 1-3 Validation 리펙토링
+     * @Valid 어노테이션 추가
+     * @param authUser
+     * @param userChangePasswordRequest
+     */
+    // auth는 임의로 만든 어노테이션
+    // 권한 확인용?
     @PutMapping("/users")
-    public void changePassword(@Auth AuthUser authUser, @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
+    public void changePassword(@Auth AuthUser authUser, @Valid @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
         userService.changePassword(authUser.getId(), userChangePasswordRequest);
     }
 }
